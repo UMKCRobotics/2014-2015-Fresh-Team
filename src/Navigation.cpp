299fc4e14c42342660babab29fe77c945a6eaf73
Connor <murphy18241@gmail.com>
2014-11-18 17:02:03 -0600

    Added orientation, position accessors and functionality. Also figured out what the class was supposed to be used for and implemented the *correct* functionality for it :)

--- src/Navigation.cpp
+++ src/Navigation.cpp
@@ -17,44 +17,49 @@ void Navigation::storeCriticalPath()
 
 Cardinal Navigation::getCardinalToNextNodeInPath(vector<Cardinal> openings)
 {
-	// Apply right-hand rule
-	if(openings.size() == 1) return openings[0];
-	else
-	{
-		sort(openings.begn(), openings.end(), cardinalComparator);
-
-		int endDirection = orientation - 1;
-		if(endDirection < 0) endDirection = Cardinal.West;
-
-		for(int directionIndex = orientation; endDirection; directionIndex++)
-		{
-			if(std::find(openings.begin(), openings.end(), Cardinal(directionIndex) != openings.end())
-			{
-				// This direction exists so lets go with that.
-				return Cardinal(directionIndex);
-			}
-		}
-	}
-
-	// No openings in the front 3 sides of the robot
-	// Therefore we are in a dead end and we should just turn around
-	int deadendExitDirection = orientation + 2;
-	if(deadendExitDirection > 3) deadendExitDirection -= 3;
-
-	return Cardinal(deadendExitDirection);
+	return NORTH;
 }
 
-void Navigation::addCurrentNodePlusCardinalToPath()
+void Navigation::addCurrentNodePlusCardinalToPath(Cardinal node)
 {
-	
+	map[position] = node;
 }
 
 bool Navigation::inFinalNode()
 {
-
+	return position == finalPosition;
 }
 
-bool Navigation::cardinalComparator(Cardinal a, Cardinal b)
+void Navigation::setRoundAndPart(int round, int part)
 {
-	return ( (a - b) < 0 ); // Equivalent of a < b
+	if(round == 1)
+	{
+		position = 48;
+		finalPosition = 9;
+	}
+	else if(round == 2) 
+	{
+		position = 48;
+		finalPosition = 1;
+	}
+	else if(round == 3)
+	{
+		position = 49;
+		finalPosition = 1;
+	}
+}
+
+int Navigation::getCurrentPosition()
+{
+	return position;
+}
+
+int Navigation::getFinalPosition()
+{
+	return finalPosition;
+}
+
+Cardinal Navigation::getOrientation()
+{
+	return orientation;
 }
--- src/Navigation.h
+++ src/Navigation.h
@@ -13,15 +13,23 @@ class Navigation
 private:
 	const string fileLocation = "/home/Ubuntu/Documents/path.txt";
 	Cardinal orientation;
+	Cardinal map[50];
 
-	bool cardinalComparator(Cardinal a, Cardinal b);
+	int position;
+	int finalPosition;
+
 public:
 	Navigation();
 	Cardinal getCardinalToNextNodeInPath(vector<Cardinal> openings);
 	void loadPath();
 	void storeCriticalPath();
-	void addCurrentNodePlusCardinalToPath();
+	void addCurrentNodePlusCardinalToPath(Cardinal node);
 	bool inFinalNode();
+
+	void setRoundAndPart(int round, int part);
+	int getCurrentPosition();
+	int getFinalPosition();
+	Cardinal getOrientation(void);
 };
 
 #endif
