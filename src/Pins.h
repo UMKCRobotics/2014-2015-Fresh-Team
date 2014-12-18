36e58aa56a3ae0ddd7cce8f778137305de32db76
Connor <murphy18241@gmail.com>
2014-12-17 19:19:19 -0600

    Updated pin configurations to reflect assembly of robot

--- src/Pins.h
+++ src/Pins.h
@@ -22,16 +22,47 @@ Input Pins
 - Reading current round and part
 - Telling the robot to start
 ********************************************/
-const int PIN_GO_BUTTON 		= 10;
-const int PIN_ROUND_SELECT 		= 11;
-const int PIN_PART_SELECT		= 12;
+const int PIN_GO_BUTTON_TO 					= 162;
+const int PIN_GO_BUTTON_FROM 				= 160;
+const int PIN_GO_BUTTON_POWER 				= 161;
+const int PIN_GO_BUTTON_GND 				= 150;
+
+const int PIN_ROUND_SELECT_VCC 				= 16;
+const int PIN_ROUND_SELECT_SIGNAL 			= 11;
+const int PIN_ROUND_SELECT_GND 				= 4;
+
+const int PIN_PART_SELECT_VCC				= 40;
+const int PIN_PART_SELECT_SIGNAL			= 3;
+const int PIN_PART_SELECT_GND				= 3;
+
+/********************************************
+Distance Sensors
+- Arranged in the following manner:
+
+    ^FRONT^
+
+    FL  FR
+  LF      RF
+
+  LB      RB
+
+- Includes their GND, VCC, and signal pins   
+********************************************/
+
 
 /********************************************
 Output Pins
 - LED's for showing robot state
+- Also includes their GND pins
+- If you're not an electrical: an LED just
+  needs a potential difference, not an actual
+  ground to emit light
 ********************************************/
-const int PIN_READY				= 13;
-const int PIN_END				= 14;
+const int PIN_READY_LIGHT_VCC			= 13;
+const int PIN_READY_LIGHT_GND 			= 13;
+
+const int PIN_END_LIGHT_VCC				= 13;
+const int PIN_END_LIGHT_GND 			= 13;
 
 /********************************************
 Pin Settings
