4cb509d4f8de7f5590802f5caf0a4ca45273d2e7
Connor <murphy18241@gmail.com>
2015-01-30 17:43:08 -0600

    Implemented necessities of line and distance sensor commands. Calibration of thresholds still necessary

--- Arduino/src/sketch.ino
+++ Arduino/src/sketch.ino
@@ -1,5 +1,19 @@
-char incomingByte = '';
-string receivedMessage = "";
+#include "string.h"
+
+#include "../../src/Pins.h"
+#include "LineSensor.h"
+
+// --- Message Handling ---
+char incomingByte;
+String receivedMessage;
+void handleMesage(String message);
+
+// --- Line Sensors ---
+LineSensor ls_left;
+LineSensor ls_right;
+
+// --- IMU ---
+// TODO
 
 void setup()
 {
@@ -18,15 +32,26 @@ void loop()
 
 	// If we actually got more than a character, parse it
 	// If not, just throw it away
-	if(strlen(receivedMessage) > 1)
+	if(strlen(receivedMessage.c_str()) > 1)
 	{
 		handleMessage(receivedMessage);
 		receivedMessage = ""; // Still need to clear the message for next time
 	} 
-	else 
+	else
 	{
 		receivedMessage = "";
 	}
+
+	// Update 
+	ls_left.update(analogRead(PIN_LINE_SENSOR_L));
+	ls_right.update(analogRead(PIN_LINE_SENSOR_R));
+
+	// Check and see if both of them have seen a line  within
+	// the threshold of time
+	if(ls_left.lineDetected(ls_right.getTimeDetected()))
+	{
+		Serial.println("LineDetected");
+	}	
 }
 
 // Handles a message sent by the Linux side to the Arduino side
@@ -35,14 +60,38 @@ void loop()
 // message will be handled inside the class through
 // the serial object
 // Handling of errors will also occur inside
-void handleMessage(string message)
+void handleMessage(String message)
 {
-	string response = "";
+	String response;
 
-	if(message == "readRoundAndPart")
+	if(response.startsWith("ReadDistSensor "))
 	{
-		response = "1:1"
+		String sensor = message.substring(11);
+
+		if(sensor == "F")
+		{
+			response = "DistSensor " + sensor + " " + analogRead(PIN_DISTANCE_SENSOR_F);
+		}
+		else if(sensor == "LF")
+		{
+			response = "DistSensor " + sensor + " " + analogRead(PIN_DISTANCE_SENSOR_LF);
+		}
+		else if(sensor == "LB")
+		{
+			response = "DistSensor " + sensor + " " + analogRead(PIN_DISTANCE_SENSOR_LB);
+		}
+		else if(sensor == "RF")
+		{
+			response = "DistSensor " + sensor + " " + analogRead(PIN_DISTANCE_SENSOR_RF);
+		}
+		else if(sensor == "RB")
+		{
+			response = "DistSensor " + sensor + " " + analogRead(PIN_DISTANCE_SENSOR_RB);
+		}
 	}
 
-	Serial.println(response);
+	if(strlen(response.c_str()) > 1)
+	{
+		Serial.println(response);
+	}
 }
