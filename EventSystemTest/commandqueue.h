a6813ceb6529df74a06687915bc0de02c034ba86
josh <joshualherndon@gmail.com>
2015-01-31 19:41:16 -0600

    commandqueue compiles once more\!

--- EventSystemTest/commandqueue.h
+++ EventSystemTest/commandqueue.h
@@ -1,14 +1,17 @@
 #ifndef COMMANDQUEUE_H
 #define COMMANDQUEUE_H
 
+#include <iostream>
 #include <queue>
 #include <functional>
 #include <vector>
 #include <memory>
 #include <map>
+#include <utility>
 #include "command.h"
 
 
+template<typename CommandType>
 class commandqueue
 {
 private:
@@ -19,24 +22,56 @@ private:
 	std::map<CommandType, std::function<void()>> FunctionMap;
 
 	//priority queue accessors
-	void push(command a);
-	command poptop();
+	void push(command a)
+	{
+		commands.push(a);
+	}
+
+	command poptop()
+	{
+		command c;
+		
+		if(!commands.empty())
+		{
+			c = commands.top();
+			commands.pop();
+
+			return c;
+		}
+		
+		c.priority = 999;
+		c.dothis = [](){std::cout << "No available commands in commandqueue\n";}; //switch this to logger
+		c.commandtype = "EMPTY";
+		return c;
+	}
 	
 	//constructor
 	commandqueue()
 	{
 		std::function<bool(command,command)> compare = [](command a, command b){return a.priority > b.priority;};
 		commands = std::priority_queue<command, std::vector<command>, decltype(compare)>(compare);
-	};
+	}
 
 	//made up my own word "Registree"
 	template<typename Registree>
-	void map(CommandType& commandtype, Registree&& registree);
+	void map(const CommandType& commandtype, const Registree&& registree)
+	{
+		FunctionMap.emplace(commandtype, registree);
+	}
 
 public:
 
-	static void sendCommand(command a);
-	static command receiveCommand();
+	static void sendCommand(command a)
+	{
+		commandqueue::getinstance().push(a);
+	}
+
+	
+	static command receiveCommand()
+	{
+		return commandqueue::getinstance().poptop();
+	}
+
 	static commandqueue& getinstance()
 	{
 		static commandqueue instance;
@@ -44,7 +79,10 @@ public:
 	}
 
 	template<typename Registree>
-	static void register(CommandType& commandtype, Registree&& registree);
+	static void registerObject(CommandType& commandtype, Registree&& registree)
+	{
+		commandqueue::getinstance().map(commandtype, registree);
+	}
 };
 
 #endif
