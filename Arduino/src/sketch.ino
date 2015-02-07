f7b424d0d5deb6a0f08a531507b184b62807d906
Connor <murphy18241@gmail.com>
2015-02-06 23:09:06 -0600

    Added code for IMU turn handling

--- Arduino/src/sketch.ino
+++ Arduino/src/sketch.ino
@@ -2,6 +2,7 @@
 
 #include "../../src/Pins.h"
 #include "LineSensor.h"
+#include "IMU.h"
 
 // --- Message Handling ---
 char incomingByte;
@@ -13,7 +14,8 @@ LineSensor ls_left;
 LineSensor ls_right;
 
 // --- IMU ---
-// TODO
+LSM9DS0 IMU(MODE_I2C, 0x6B, 0x1D); 	// Arguments are from SparkFun example
+									// See "Simple" example for explanation
 
 void setup()
 {
@@ -88,6 +90,36 @@ void handleMessage(String message)
 		{
 			response = "DistSensor " + sensor + " " + analogRead(PIN_DISTANCE_SENSOR_RB);
 		}
+	}
+	else if(response.startsWith("NotifyOfAngle "))
+	{
+		// This clause will send a message to the Linux side to stop turning when a designated
+		// angle has been reached (such as 90 or 180 degrees)
+
+		// Parse end angle from string
+		String str_endAngle = response.substring(14);
+		float f_endAngle = str_angle.toFloat();
+
+		long lastCheckTime = microseconds();
+		float currentRotationSpeed = 0;
+		long timeSinceLastCheck = 0;
+		float f_angle = 0.0f;
+
+		// While the difference between the current angle and end angle is +/- 3 degrees...
+		while(fabs(angle - f_endAngle) >= 3) // 3 is the error in degrees acceptable. Will need to be tested
+		{
+			// Get our rotation speed and get an estimation as to how long we have been
+			// turning at that speed
+			currentRotationSpeed = IMU.readGyro(IMU.gz);
+			timeSinceLastCheck = microseconds() - lastCheckTime;
+
+			// Add the degrees turned during that time period to our current angle
+			// Also set the lastCheckTime for the next calculation
+			angle += timeSinceLastCheck * currentRotationSpeed;
+			lastCheckTime = microseconds();
+		}
+
+		Serial.println("AngleReached");
 	}
 
 	if(strlen(response.c_str()) > 1)
