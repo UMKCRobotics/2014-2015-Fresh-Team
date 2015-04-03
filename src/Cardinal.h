977f644e0e4a678e263950416c148f6b9d5e853d
Ubuntu <notset>
2015-04-03 20:10:49 +0000

    Assigned constants to directions to use with turn calculation

--- src/Cardinal.h
+++ src/Cardinal.h
@@ -1,10 +1,10 @@
 #ifndef CARDINAL_H
 #define CARDINAL_H
 
-enum Cardinal { EAST,
-		NORTH,
-		WEST,
-		SOUTH
+enum Cardinal { EAST = 0, // East is terminal angle of 0 degress of the standard unit circle
+		NORTH = 1,
+		WEST = 2,
+		SOUTH = 3
 };
 
 #endif
