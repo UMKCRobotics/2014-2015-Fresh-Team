e945e9a0e6441bba4df6d4c5295decc466930bde
Ubuntu <notset>
2015-04-11 21:24:45 +0000

    Added cardinal names array to resolve enums to strings easily

--- src/Cardinal.h
+++ src/Cardinal.h
@@ -1,10 +1,14 @@
 #ifndef CARDINAL_H
 #define CARDINAL_H
 
-enum Cardinal { EAST = 0, // East is terminal angle of 0 degress of the standard unit circle
+enum Cardinal {
+		EAST = 0, // East is terminal angle of 0 degress of the standard unit circle
 		NORTH = 1,
 		WEST = 2,
-		SOUTH = 3
+		SOUTH = 3,
+		SIZE_OF_ENUM
 };
+
+static const char* CardinalNames[SIZE_OF_ENUM] = {"EAST", "NORTH", "WEST", "SOUTH"};
 
 #endif
